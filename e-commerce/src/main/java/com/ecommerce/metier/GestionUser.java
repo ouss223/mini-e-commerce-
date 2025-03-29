acd45de1f05b0a6b1eddaae4758f35c3f1c6ae46
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2025-03-29 16:23:10 +0100

    add password validation

--- e-commerce/src/main/java/com/ecommerce/metier/GestionUser.java
+++ e-commerce/src/main/java/com/ecommerce/metier/GestionUser.java
@@ -3,24 +3,28 @@ package com.ecommerce.metier;
 import com.ecommerce.dao.GestionUserDAO;
 import com.ecommerce.dao.IGestionUserDAO;
 import com.ecommerce.model.User;
+import com.ecommerce.utils.FunctionsUtils;
 import java.sql.SQLException;
 
 public class GestionUser implements IGestionUser {
-    private IGestionUserDAO userDAO = new GestionUserDAO();
+    private  IGestionUserDAO userDAO = new GestionUserDAO();
 
     @Override
     public void registerUser(User user) throws SQLException {
-        userDAO.registerUser(user);
+        // Validate password before registering
+        if (FunctionsUtils.isValidPassword(user.getPassword())) {
+            userDAO.registerUser(user);  //todo : add hashing
+        } else {
+            throw new IllegalArgumentException("Password does not meet the required criteria.");
+        }
     }
 
     @Override
     public User authenticateUser(String email, String password) throws SQLException {
         User user = userDAO.getUserByEmail(email);
-        if (user != null && user.getPassword().equals(password)) {
-            return user; //todo : add hadhing
+        if (user != null && user.getPassword().equals(password)) {  //todo : add hashing
+            return user;
         }
         return null;
     }
 }
-
-
--- e-commerce/src/main/java/com/ecommerce/utils/FunctionsUtils.java
+++ e-commerce/src/main/java/com/ecommerce/utils/FunctionsUtils.java
@@ -1,10 +1,26 @@
 package com.ecommerce.utils;
 
+import java.util.regex.Pattern;
+
 public class FunctionsUtils {
+
     public static String Slice(String text, int n) {
         if (text == null || text.isEmpty() || n <= 0) {
             return "";
         }
         return text.length() <= n ? text : text.substring(0, n) + "...";
+    }
+
+
+
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$";
+
+    public static boolean isValidPassword(String password) {
+        if (password == null) {
+            return false;
+        }
+
+        return Pattern.matches(PASSWORD_PATTERN, password);
     }
 }
